c89ac15e07500ae9a0fb8542f05ca18ad32c266a
Linus Fredriksson <linfredriksson@gmail.com>
2015-10-02 10:55:26 +0200

    - Add function for updating node cost. - Add empty step function for AStar.

--- src/pathfinding.cpp
+++ src/pathfinding.cpp
@@ -6,11 +6,25 @@ namespace PathFinding
 	{
 		struct Node
 		{
-			int x, y, id;
+			int x, y;
 			float costH, costG, costF;
 			int parent;
 		};
+
+		void updateCost(Node &node, Node &parent)
+		{
+			node.costG = parent.costG != 0 ? parent.costG + 10.0f : 0.0f;
+			node.costF = node.costH + node.costG;
+		}
+
+		void AstarStep()
+		{
+
+		}
 	}
 
-	void AStar();
+	void AStar()
+	{
+
+	}
 };
