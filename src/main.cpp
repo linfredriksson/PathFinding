35c6d504854edc97fe481eb17b590450fad71366
Linus Fredriksson <linfredriksson@gmail.com>
2015-10-04 19:36:06 +0200

    - Implement AStarStep function. Not 100% complete.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include "astar.h"
+#include "pathfinding.h"
 #include "maze.h"
 
 int main()
 {
-	int width = 4;
-	int height = 3;
+	int width = 11;
+	int height = 11;
 	unsigned char *map = new unsigned char[width * height];
 	Maze::PrimsAlgorithm(width, height, map);
 	Maze::printToConsole(width, height, map);
+
+	PathFinding::AStar(1, 1, 9, 9, *map, width, height);
+
+	int a; std::cin >> a; // pause program
 
 	return 0;
 }
--- src/pathfinding.cpp
+++ src/pathfinding.cpp
@@ -1,6 +1,9 @@
 #include "pathfinding.h"
 #include <math.h>
 #include <vector>
+#include <algorithm>
+
+#include <iostream>
 
 namespace PathFinding
 {
@@ -12,7 +15,7 @@ namespace PathFinding
 			float costG, costF, costH;
 			int parent;
 
-			inline bool operator < (const Node &rhs) { return costF < rhs.costF; }
+			inline bool operator < (const Node &rhs) { return costF > rhs.costF; }
 			inline bool operator == (const Node &rhs) { return x == rhs.x && y == rhs.y; }
 		};
 
@@ -38,14 +41,63 @@ namespace PathFinding
 			updateCostGF(node, parentCostG);
 		}
 
-		void AStarStep(list &open, list &closed, list &path)
+		bool AStarStep(const unsigned char &	map, int mapWidth, int mapHeight,
+			const Node &target, list &open, list &closed, list &path)
 		{
 			if (open.size() == 0)
-				return;
+				return false;
 
-			open.pop_back(); // tmp
+			std::sort(open.begin(), open.end());
 
-			AStarStep(open, closed, path);
+			closed.push_back(open.back());
+			open.pop_back();
+			Node *currentNode = &closed.back();
+			
+			int neighbour[4][2] = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+			for (int i = 0; i < 4; ++i)
+			{
+				int posX = (*currentNode).x + neighbour[i][0];
+				int posY = (*currentNode).y + neighbour[i][1];
+
+				// check if neighbour is out of bounds
+				if (posX < 0 || posY < 0 || posX > mapWidth || posY > mapHeight)
+					continue;
+
+				// check if neighbour is a wall
+				if ((&map)[posX + posY * mapWidth])
+					continue;
+
+				Node temp;
+				initiateNode(temp, posX, posY, closed.size() - 1);
+
+				// check if neighbour already is in the closed list
+				if (std::find(closed.begin(), closed.end(), temp) != closed.end())
+					continue;
+
+				// check if neighbour already is in the open list
+				auto it = std::find(open.begin(), open.end(), temp);
+
+				if (it != open.end())
+				{
+					if (currentNode->costG + 10 < it->costG)
+					{
+						// find currentNodes position in the closed list.
+						// set this position as the temp nodes parent.
+						// update cost.
+					}
+
+					continue;
+				}
+
+				open.push_back(temp);
+
+				if (temp == target)
+				{
+					return true;
+				}
+			}
+
+			return AStarStep(map, mapWidth, mapHeight, target, open, closed, path);
 		}
 	}
 
@@ -64,6 +116,11 @@ namespace PathFinding
 		updateCostGFH(start, 0.0f, target);
 		open.push_back(start);
 
-		AStarStep(open, closed, path);
+		bool result = AStarStep(map, mapWidth, mapHeight, target, open, closed, path);
+
+		if (result)
+			std::cout << "PATH FOUND :)" << std::endl;
+		else
+			std::cout << "NO PATH FOUND :(" << std::endl;
 	}
 };
